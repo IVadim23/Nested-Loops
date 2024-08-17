f54b2cbd1d207e1bad980cb11d6c0f3186f9f5db
IVadim23 <inazrski@gmail.com>
2024-08-18 00:04:00 +0300

    Add files via upload

---
+++ 03. Combinations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    internal class Program
+    {
+        private static int n;
+
+        static void Main(string[] args)
+        {
+           int q = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= q; i++)
+            {
+                for (int b = 0; b <= q; b++)
+                {
+                    for (int c = 0; c <= q; c++)
+                    {
+                       if (i +b+c == q)
+                        {
+                            Console.WriteLine(i +b+c);
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
