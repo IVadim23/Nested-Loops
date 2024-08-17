d4c3ee7f03f1ba7cb6e70727868320a041efe9f5
IVadim23 <inazrski@gmail.com>
2024-08-18 00:06:35 +0300

    Add files via upload

---
+++ 04. Sum of Two NumbersD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P04.SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start of the interval
+            int start = int.Parse(Console.ReadLine());
+            //End of the interval
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+
+            int combinationCnt = 0;
+            bool comboFound = false;
+            for (int firstNum = start; firstNum <= end; firstNum++)
+            {
+                for (int secondNum = start; secondNum <= end; secondNum++)
+                {
+                    //One Combination -> {firstNum} {secondNum}
+                    combinationCnt++;
+
+                    if (firstNum + secondNum == magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{combinationCnt} ({firstNum} + {secondNum} = {magicNum})");
+                        comboFound = true;
+                        break;
+                    }
+                }
+
+                if (comboFound)
+                {
+                    break;
+                }
+            }
+
+            if (!comboFound)
+            {
+                Console.WriteLine($"{combinationCnt} combinations - neither equals {magicNum}");
+            }
+        }
+    }
+}
